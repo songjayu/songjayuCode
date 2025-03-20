699f53db53e189f62893ad93b29c9abce1303269
songjayu <137712016+songjayu@users.noreply.github.com>
2025-03-20 23:04:14 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,15 +1,10 @@
 def find_layer(N):
     if N == 1:
         return 1
-    
     layer = 1
     maxNum = 1
-
     while maxNum < N:
         maxNum += 6 * layer
         layer += 1
-    
     return layer
-
-
 print(find_layer(int(input())))
